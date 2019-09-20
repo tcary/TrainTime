26050047cf783e4d609a190336c9a8c192dc9822
Tetiana Cary <taniushka.tata@gmail.com>
2019-09-20 09:36:59 -0600

    Added childSnapshot function

--- assets/app.js
+++ assets/app.js
@@ -12,44 +12,64 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-// $("#add-train").on("click", function (event) {
-//     event.preventDefault();
-var trainName = $("train-name-input").val().trim();
-var destination = $("dest-input").val().trim();
-var firstTrain = $("start-input").val().trim();
-var frequency = $("frequency-input").val().trim();
+$("#add-train-btn").on("click", function (event) {
+    event.preventDefault();
 
-var newTrain = {
-    name: trainName,
-    dest: destination,
-    first: firstTrain,
-    often: frequency,
-}
-database.ref().push(newTrain);
+    var trainName = $("#train-name-input").val().trim();
+    var destination = $("#dest-input").val().trim();
+    var firstTrain = $("#start-input").val().trim();
+    var frequency = $("#frequency-input").val().trim();
 
-console.log(newTrain.name);
-console.log(newTrain.dest);
-console.log(newTrain.first);
-console.log(newTrain.often);
+    var newTrain = {
+        name: trainName,
+        dest: destination,
+        first: firstTrain,
+        often: frequency,
+    }
+    database.ref().push(newTrain);
 
-alert("Train successfully added");
+    console.log(newTrain.name);
+    console.log(newTrain.dest);
+    console.log(newTrain.first);
+    console.log(newTrain.often);
 
-$("#train-name-input").val("");
-$("#dest-input").val("");
-$("#start-input").val("");
-$("#frequency-input").val("");
+    alert("Train successfully added");
+
+    $("#train-name-input").val("");
+    $("#dest-input").val("");
+    $("#start-input").val("");
+    $("#frequency-input").val("");
+})
 
 
 
 database.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val());
-    //     var row = $("<tr>");
-    //     var column1 = $("<td>").text(childSnapshot.val().name)
-    //     var column2 = $("<td>").text(childSnapshot.val().role)
-    //     var column3 = $("<td>").text(childSnapshot.val().startDate)
-    //     var column4 = $("<td>").text(childSnapshot.val().endDate)
-    //     var column5 = $("<td>").text(childSnapshot.val().monthlyRate)
-    //     var column6 = $("<td>").text(childSnapshot.val().totalBilled)
-    // }), function (errorObj) {
-    //     consol.log("Something went wrong....error code: " + errorObj.code);
+
+    var trainName = childSnapshot.val().name;
+    var destination = childSnapshot.val().dest;
+    var firstTrain = childSnapshot.val().first;
+    var frequency = childSnapshot.val().often;
+
+    var convertedTime = moment(firstTrain, "HH:mm").subtract(1, "year");
+    var diffTime = moment().diff(moment(convertedTime), "minutes");
+    var remainder = diffTime % frequency;
+    var away = frequency - remainder;
+    var next = moment().add(away, "minutes").format("HH:mm");
+
+    console.log(convertedTime, diffTime, remainder, away, next)
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(next),
+        $("<td>").text(away),
+    )
+    $("#train-table").append(newRow);
+
+
+
+
+
 })
